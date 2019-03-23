5aed2ae22d3a75985b5b100127b92af0dee6e67b
Rafal Miodowski <rafalmiodowski@gmail.com>
2019-03-24 00:54:49 +0100

    Create Footer component

--- components/base/Footer.vue
+++ components/base/Footer.vue
@@ -3,27 +3,27 @@
     <logo/>
     <ul class="links">
       <li class="links__link">
-        <nuxt-link to="">
+        <nuxt-link to="" class="text-link">
           Obserwacja
         </nuxt-link>
       </li>
       <li class="links__link">
-        <nuxt-link to="">
+        <nuxt-link to="" class="text-link">
           Kontrobserwacja
         </nuxt-link>
       </li>
       <li class="links__link">
-        <nuxt-link to="">
+        <nuxt-link to="" class="text-link">
           Blog
         </nuxt-link>
       </li>
       <li class="links__link">
-        <nuxt-link to="">
+        <nuxt-link to="" class="text-link">
           O detektywie
         </nuxt-link>
       </li>
       <li class="links__link">
-        <nuxt-link to="">
+        <nuxt-link to="" class="text-link">
           Kontakt
         </nuxt-link>
       </li>
@@ -33,36 +33,59 @@
         detektyw
       </span>
       <hr class="info__line">
-      <span class="info__text">
+      <h4 class="info__text">
         Kamil Mikrut
-      </span>
+      </h4>
       <span>
         koncesja MSWiA
       </span>
       <hr class="info__line">
-      <span>
+      <h4>
         RD-19/2016
-      </span>
+      </h4>
       <span>
         nr licencji
       </span>
       <hr class="info__line">
-      <span>
+      <h4>
         0001899
-      </span>
+      </h4>
     </div>
+    <social all boxed/>
+    <small class="copy">{{ `&copy; ${ currentYear }, Kamil Mikrut ` }}</small>
   </footer>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      currentYear: new Date().getFullYear()
+    }
+  }
+}
+</script>
 
 
 <style lang="scss" scoped>
   .footer {
     @include main-container;
-    border: 1px solid gray;
-    font-size: 14px;
-    font-weight: 700;
+    padding-top: 32px;
+    padding-bottom: 160px;
     display: flex;
     justify-content: space-between;
+
+    @include media-down(md) {
+      flex-direction: column;
+      padding-bottom: 96px;
+      & > *:not(:last-child) {
+        margin-bottom: 64px;
+      }
+    }
+
+    font-size: 14px;
+    font-weight: 700;
+    position: relative;
   }
 
   .links {
@@ -82,6 +105,21 @@
     grid-gap: 32px;
     &__line {
       align-self: center;
+    }
+  }
+
+  .copy {
+    font-size: 12px;
+    letter-spacing: 1.1px;
+    font-weight: 500;
+    position: absolute;
+    margin-bottom: 16px;
+    bottom: 0;
+    right: $container-padding;
+
+    @include media-down(md) {
+      right: unset;
+      left: $container-padding;
     }
   }
 </style>
