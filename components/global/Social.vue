daab263adb38a0e5f95d2e7a0ee2b9f36891c31d
Rafal Miodowski <rafalmiodowski@gmail.com>
2019-03-23 21:47:08 +0100

    Register global components

---
+++ components/global/Social.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="socials">
+    <li class="socials__icon">
+      lol
+    </li>
+    <li class="socials__icon">
+      <svg
+        width="8px"
+        height="16px"
+        viewBox="0 0 8 16"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
+        <title>Facebook</title>
+        <path
+          stroke="none"
+          stroke-width="1"
+          fill="#000000"
+          fill-rule="evenodd"
+          d="M19.0819672,15.9781421 L16.8961749,15.9781421 L16.8961749,24 L13.5737705,24 L13.5737705,15.9781421 L12,15.9781421 L12,13.1584699 L13.5737705,13.1584699 L13.5737705,11.3442623 C13.5737705,10.0327869 14.1857923,8 16.9180328,8 L19.3661202,8 L19.3661202,10.7322404 L17.5737705,10.7322404 C17.2896175,10.7322404 16.8743169,10.8852459 16.8743169,11.4972678 L16.8743169,13.1584699 L19.3442623,13.1584699 L19.0819672,15.9781421 Z"
+        />
+      </svg>
+    </li>
+    <li class="socials__icon">
+      lol
+    </li>
+  </ul>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+  .socials {
+    &__icon {
+      width: 32px;
+      height: 32px;
+      background: $secondary-color;
+      @include center-content;
+    }
+  }
+</style>
+
