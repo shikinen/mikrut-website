7d118bbbba3ed0c345db8b34051a0e0382170186
Rafal Miodowski <rafalmiodowski@gmail.com>
2019-03-25 10:33:14 +0100

    Create section title

---
+++ components/base/SectionTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <h3 class="section-title">
+    {{ title }}
+  </h3>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .section-title {
+    font-size: 80px;
+    font-weight: 700;
+  }
+</style>
