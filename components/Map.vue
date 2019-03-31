56949d3bef9f02334cce77e45e20307e1017ad2f
Rafal Miodowski <rafalmiodowski@gmail.com>
2019-03-31 13:17:14 +0200

    Create map section

---
+++ components/Map.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="map">
+    <div>
+      <section-title title="Obszar operacyjny" underline left/>
+      <p class="map__text subtitle-medium">
+        {{ map }}
+      </p>
+    </div>
+    <img src="/img/map.svg">
+  </section>
+</template>
+
+<script>
+import SectionTitle from '@/components/base/SectionTitle'
+import data from '~/data/db.json'
+
+export default {
+  components: {
+    SectionTitle
+  },
+  data () {
+    return {
+      map: data.map
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .map {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-column-gap: 16px;
+
+    &__text {
+      margin-top: 96px;
+      margin-bottom: 72px;
+      max-width: 378px;
+    }
+
+  }
+</style>
