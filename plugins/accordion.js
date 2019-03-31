781565a4e7697d70df7fc2e182c0659d3919ce15
Rafal Miodowski <rafalmiodowski@gmail.com>
2019-03-31 13:17:38 +0200

    Install mock accordion

---
+++ plugins/accordion.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import { BulmaAccordion, BulmaAccordionItem } from 'vue-bulma-accordion'
+
+Vue.component('accordion', BulmaAccordion)
+Vue.component('accordion-item', BulmaAccordionItem)
