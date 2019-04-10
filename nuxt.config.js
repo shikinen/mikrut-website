f9086b44dec806c011706fccb15f742771fcf521
Rafal Miodowski <rafalmiodowski@gmail.com>
2019-04-10 19:14:42 +0200

    Fix scroll behaviour in router

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,11 +32,24 @@ export default {
   */
   router: {
     scrollBehavior(to) {
-      if (to.hash) {
+      if (to.hash && document.querySelector(to.hash)) {
         return window.scrollTo({ top: document.querySelector(to.hash).offsetTop + window.innerHeight, behavior: 'smooth' });
       }
-      return window.scrollTo({ top: 0, behavior: 'smooth' });
-      }
+      let position = false
+      return new Promise(resolve => {
+        // wait for the out transition to complete (if necessary)
+        window.$nuxt.$once('triggerScroll', () => {
+          // coords will be used if no selector is provided,
+          // or if the selector didn't match any element.
+          if (to.hash && document.querySelector(to.hash)) {
+            // scroll to anchor by returning the selector
+            // return window.scrollTo({ top: document.querySelector(to.hash).offsetTop + window.innerHeight, behavior: 'smooth' });
+            resolve({ x: 0, y: document.querySelector(to.hash).offsetTop + window.innerHeight })
+          }
+          return window.scrollTo({ top: 0, behavior: 'smooth' });
+        })
+      })
+    }
   },
 
   /*
@@ -58,8 +71,8 @@ export default {
     '~plugins/global-components',
     '~plugins/vue-lazyload',
     '~plugins/directives',
-    { src:'~/plugins/lax.js', ssr: false },
-    { src: "~/plugins/aos", ssr: false }
+    { src:'~/plugins/lax.js', ssr: false }
+    // { src: "~/plugins/aos", ssr: false }
   ],
 
   /*
@@ -77,6 +90,8 @@ export default {
           iso: 'pl'
         }
       ],
+      strategy: 'prefix_except_default',
+      parsePages: false,
       defaultLocale: 'pl',
       vueI18n: {
         fallbackLocale: 'pl',
